98d93739c81951d845b47f57bd5f75055bb4108c
Shane Riley <shane@shaneriley.com>
2012-06-16 09:17:24 -0400

    Better: Pass form element as argument

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -14,27 +14,28 @@ $(function() {
     return blank;
   };
 
-  var $form = $("form");
+  (function($form) {
+    $form.submit(function(e) {
+      var errors = $form.find(".error").length;
+      errors && e.preventDefault();
+    });
 
-  $form.submit(function(e) {
-    var errors = $form.find(".error").length;
-    errors && e.preventDefault();
-  });
+    $form.find(":password").blur(function() {
+      var $pwds = $form.find(":password");
+      $form.find(".error#password").remove();
+      if (isIncomplete($pwds)) { return; }
+      if ($pwds.eq(0).val() !== $pwds.eq(1).val()) {
+        newError("Passwords must match", "password").insertBefore($pwds[1]);
+      }
+    });
 
-  $form.find(":password").blur(function() {
-    var $pwds = $form.find(":password");
-    $form.find(".error#password").remove();
-    if (isIncomplete($pwds)) { return; }
-    if ($pwds.eq(0).val() !== $pwds.eq(1).val()) {
-      newError("Passwords must match", "password").insertBefore($pwds[1]);
-    }
-  });
+    $form.find(":text, :email, :password").blur(function() {
+      $form.find(":submit").prop("disabled", isIncomplete($form.find(":text, :email, :password")));
+    });
 
-  $form.find(":text, :email, :password").blur(function() {
-    $form.find(":submit").prop("disabled", isIncomplete($form.find(":text, :email, :password")));
-  });
+    $form.find(":submit").prop("disabled", true);
+  })($("form"));
 
-  $form.find(":submit").prop("disabled", true);
 });
 
 (function($) {
